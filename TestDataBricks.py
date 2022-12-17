8aa6b9a81f65c1ec1ed2bc11d48534eb5275cc54
premanand.sreedharan@gmail.com <premanand.sreedharan@gmail.com>
2022-12-17 15:54:31 +0000

    4th commit

--- TestDataBricks.py
+++ TestDataBricks.py
@@ -8,4 +8,24 @@ print("hello")
 
 # COMMAND ----------
 
+dbutils.fs.unmount("/mnt/dbsmount/raw")
+dbutils.fs.unmount("/mnt/dbsmount/processed")
 
+# COMMAND ----------
+
+configs = {"fs.azure.account.auth.type": "OAuth",
+          "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+          "fs.azure.account.oauth2.client.id": "bd6eca9a-bdbd-4f59-bbf4-b57801cb6125",
+          "fs.azure.account.oauth2.client.secret": "ACR8Q~sdx5aEg_M.Awl_GeqpJduxeVelvnC69akQ",
+          "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/e10b6b02-6085-4912-9810-31f6fc637e88/oauth2/token"}
+
+dbutils.fs.mount(
+  source = "abfss://raw@azuredatabricksstgacc.dfs.core.windows.net/",
+  mount_point = "/mnt/dbsmount/raw",
+  extra_configs = configs)
+
+
+dbutils.fs.mount(
+  source = "abfss://processed@azuredatabricksstgacc.dfs.core.windows.net/",
+  mount_point = "/mnt/dbsmount/processed",
+  extra_configs = configs)
