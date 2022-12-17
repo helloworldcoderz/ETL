85bff096165d9b3a1277ca59dec7f1034408465b
premanand.sreedharan@gmail.com <premanand.sreedharan@gmail.com>
2022-12-17 15:50:45 +0000

    3rd commit
    3rd Commit

--- TestDataBricks.py
+++ TestDataBricks.py
@@ -1,2 +1,11 @@
 # Databricks notebook source
 print("hello")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This is a sample workbook to read and write data from CSV to table
+
+# COMMAND ----------
+
+
